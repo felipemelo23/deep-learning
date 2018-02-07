01a61acf0107c7ab0807fcaa52174e993c1b65c2
Felipe Melo <felipemelosoares23@gmail.com>
2018-02-07 17:38:31 -0300

    Updating dataset link to ANN

--- artificial-neural-networks/ann.py
+++ artificial-neural-networks/ann.py
@@ -5,7 +5,7 @@ Created on Wed Feb 7 2018
 
 @author: Felipe Melo
 
-The dataset used here can be found at: goo.gl/2EF1tf
+The dataset used here can be found at: goo.gl/iJHJZ1
 """
 
 ### Data preprocessing ###
